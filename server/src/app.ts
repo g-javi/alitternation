850c105a52861da022b9b02ec27cb7d0a42069cf
CDBuckmaster <callumbuckmaster@gmail.com>
2018-09-08 16:17:24 +1000

    Survey data can now be retrieved from DB

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import getConfig from "./util/getConfig";
 import router from "./routes";
+import {connect} from "./util/database";
 
 
 const config = getConfig();
@@ -12,20 +13,22 @@ const mongoUri = config.DATABASE.URI;
 // PORT
 const PORT = config.SERVER.PORT;
 
-// Initialize as express application
-const app = express();
+connect().then(() => {
+    //Initialize as express application
+    const app = express();
 
-// parse request body
-app.use(bodyParser.urlencoded({ extended: true }));
+    // parse request body
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use router
-app.use(router);
+    // Use router
+    app.use(router);
 
 
-// Listen on PORT
-app.listen(config.SERVER.PORT);
+    // Listen on PORT
+    app.listen(config.SERVER.PORT);
 
-console.log('Listening on ' + config.SERVER.PORT);
+    console.log('Listening on ' + config.SERVER.PORT);
+});
 
 
 
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import LitterController from "./api/LitterController";
+import SurveyController from "./api/SurveyController";
 import auth from "./auth";
 
 
@@ -9,6 +10,7 @@ router.get("/", (req, res, next) => {
     res.send("Hello World!");
 })
 
+router.use("/survey", SurveyController);
 router.use("/litter", LitterController);
 router.use("/auth", auth);
 

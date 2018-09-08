b8efed63213b65767e8679906a56115c659bfc25
CDBuckmaster <callumbuckmaster@gmail.com>
2018-09-08 17:46:25 +1000

    Fixed a bug with BinController

--- server/src/routes/api/BinController.ts
+++ server/src/routes/api/BinController.ts
@@ -7,14 +7,14 @@ api.get("/all", (req, res, next) => {
     const bins = getDb().collection("bins");
     const query = {};
     const projection = {
-        "Geometry": 1
+        "geometry": 1
     };
     return bins.find(query, {fields:projection}).toArray()  
     .then((resArray) => {
         res.json(resArray.map((geometry) => {
             return {
-                longitude: geometry.coordinates[0],
-                latitude: geometry.coordinates[1]
+                longitude: geometry.geometry.coordinates[0],
+                latitude: geometry.geometry.coordinates[1]
             }
         }));
     })

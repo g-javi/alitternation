b31abfcae8fb5da34b4e27827b6b20d3b73b9303
James Li <manhinli@gmail.com>
2018-09-08 15:04:36 +1000

    Most of the components roughly set up with terrible styling

--- front-end/src/app/components/image-capture/image-capture.component.ts
+++ front-end/src/app/components/image-capture/image-capture.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-image-capture',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageCaptureComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly location: Location,
+  ) { }
 
   ngOnInit() {
   }
 
+  goBack() {
+    this.location.back();
+  }
+
+  captureImage() {
+    alert("Not implemented");
+    throw new Error("Not implemented");
+  }
 }

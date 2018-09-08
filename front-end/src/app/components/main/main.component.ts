eaf25b40eeca1818e2a68aa1ac5178a735f4bd53
James Li <manhinli@gmail.com>
2018-09-08 17:21:12 +1000

    Implemented maps

--- front-end/src/app/components/main/main.component.ts
+++ front-end/src/app/components/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+/// <reference types="googlemaps" />
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-main',
@@ -7,12 +8,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  @ViewChild('map') gmapElement: any;
+  map: google.maps.Map;
+  
   constructor(
     private router: Router,
   ) { }
 
   ngOnInit() {
+    const mapProp: google.maps.MapOptions = {
+      center: {lat: -27.4698, lng: 153.0251},
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      gestureHandling: "greedy",
+      disableDefaultUI: true,
+    };
+
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        this.map.setCenter(pos);
+      }, function() {
+      });
+    }
+
   }
 
   goToImageCapture() {

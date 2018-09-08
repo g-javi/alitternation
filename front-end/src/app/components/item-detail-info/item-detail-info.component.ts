46e348016924b58d208b3196fbf4f2e426299c40
James Li <manhinli@gmail.com>
2018-09-08 12:58:41 +1000

    Stripped back Material Design; Added shell components and routes

---
+++ front-end/src/app/components/item-detail-info/item-detail-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-detail-info',
+  templateUrl: './item-detail-info.component.html',
+  styleUrls: ['./item-detail-info.component.scss']
+})
+export class ItemDetailInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ front-end/src/app/components/item-lookup/item-lookup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-lookup',
+  templateUrl: './item-lookup.component.html',
+  styleUrls: ['./item-lookup.component.scss']
+})
+export class ItemLookupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

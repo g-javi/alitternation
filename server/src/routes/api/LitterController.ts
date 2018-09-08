a48c2dcc6a683b3bc4894738c52f01d1fcd5126b
Anant Tuli <mr.ananttuli@gmail.com>
2018-09-08 23:33:32 +1000

    Added routes for litter/items/id and barcode search returns ".information" property

--- server/src/routes/api/LitterController.ts
+++ server/src/routes/api/LitterController.ts
@@ -5,6 +5,18 @@ import { ObjectID } from "mongodb";
 
 const litter = express.Router();
 
+
+litter.get("/items/:id", async (req, res, next) => {
+    const litter = await getDb().collection("litter").find().toArray();
+    const item = litter.find((item: any) => item._id == req.params.id);
+    
+    if(item) {
+        res.json(item)
+    } else {
+        res.json(null);
+    }
+});
+
 litter.get("/items", async (req, res, next) => {
     const litter = await getDb().collection("litter").find().toArray();
 
@@ -17,6 +29,11 @@ litter.get("/barcode/:barcodeNumber", async (req, res, next) => {
     const barcodeItems = litter.filter((item) => item.barcode);
 
     const item = barcodeItems.find((item) => item.barcode == req.params.barcodeNumber);
+    const barcodes = await getDb().collection("barcodes").find().toArray();
+    const barcodeInformation = barcodes.find((barcodeItem) => barcodeItem.barcode == item.barcode);
+    
+    item.information = barcodeInformation;
+    
     if(item) {
         res.json(item);
     } else {
